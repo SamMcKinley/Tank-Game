7c86eb617310a5a509e1cd0de82a63f77a3e1bc7
Sam McKinley <sammckin@uat.edu>
2020-05-18 13:22:58 -0700

    Added code to the following files

--- Assets/Scripts/TankMotor.cs
+++ Assets/Scripts/TankMotor.cs
@@ -2,17 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(TankData))]
 public class TankMotor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Need a reference to the character controller component
+    private CharacterController characterController;
+    private TankData data;
+    private Transform tf;
+
+    private void Start()
     {
-        
+        characterController = gameObject.GetComponent<CharacterController>();
+        data = gameObject.GetComponent<TankData>();
+    }
+    //Handle moving the tank
+    public void Move(float speed)
+    {
+        // Create a vector to hold our speed data
+        Vector3 speedVector = tf.forward * speed;
     }
 
-    // Update is called once per frame
-    void Update()
+    //Handle rotating the tank
+    public void Rotate(float speed)
     {
-        
+        //create a vector to hold our rotation data
+        Vector3 rotateVector;
+        //Start by rotating by one degree per frame draw
+        rotateVector = Vector3.up;
+        //adjust rotation based off speed
+        rotateVector *= Time.deltaTime;
+        //pass our rotation vecctor into transform.rotate
+        transform.Rotate(rotateVector, Space.Self);
     }
 }
